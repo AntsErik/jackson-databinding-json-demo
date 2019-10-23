fe5feb4132759c539d9b67d3af85d7215e6b5d07
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-23 16:03:08 +0300

    Added new property to sample-full.json. Updated Student class to ignore the new field.

--- src/main/java/ee/praktika/jackson/json/demo/Student.java
+++ src/main/java/ee/praktika/jackson/json/demo/Student.java
@@ -1,5 +1,8 @@
 package ee.praktika.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties( ignoreUnknown = true )
 public class Student {
 
     private int id;
